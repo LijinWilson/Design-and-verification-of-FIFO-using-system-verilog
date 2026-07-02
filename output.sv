aa9e6cde9cb7979b81e9740ccf9ac43080b176a1
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-07-02 23:25:34 +0530

    Final output result after simulation

---
+++ output.sv
@@ -0,0 +1,43 @@
+# KERNEL: ASDB file was created in location /home/runner/dataset.asdb
+# KERNEL:                    1Scenario - 1
+# KERNEL:                   15Read operation >> data input = 1
+# KERNEL:                   35Read operation >> data input = 10
+# KERNEL:                   55Read operation >> data input = 100
+# KERNEL:                   86Write Operation >> Data Out = 1
+# KERNEL:                  106Write Operation >> Data Out = 10
+# KERNEL:                  126Write Operation >> Data Out = 100
+# KERNEL:                  126Scenario - 2
+# KERNEL:                  135Read operation >> data input = 1
+# KERNEL:                  166Write Operation >> Data Out = 1
+# KERNEL:                  175Read operation >> data input = 2
+# KERNEL:                  206Write Operation >> Data Out = 2
+# KERNEL:                  215Read operation >> data input = 4
+# KERNEL:                  246Write Operation >> Data Out = 4
+# KERNEL:                  255Read operation >> data input = 8
+# KERNEL:                  286Write Operation >> Data Out = 8
+# KERNEL:                  295Read operation >> data input = 16
+# KERNEL:                  326Write Operation >> Data Out = 16
+# KERNEL:                  335Read operation >> data input = 32
+# KERNEL:                  366Write Operation >> Data Out = 32
+# KERNEL:                  375Read operation >> data input = 64
+# KERNEL:                  406Write Operation >> Data Out = 64
+# KERNEL:                  415Read operation >> data input = 128
+# KERNEL:                  446Write Operation >> Data Out = 128
+# KERNEL:                  446Scenario - 3
+# KERNEL:                  455Read operation >> data input = 1
+# KERNEL:                  475Read operation >> data input = 2
+# KERNEL:                  495Read operation >> data input = 4
+# KERNEL:                  515Read operation >> data input = 8
+# KERNEL:                  535Read operation >> data input = 16
+# KERNEL:                  555Read operation >> data input = 32
+# KERNEL:                  575Read operation >> data input = 64
+# KERNEL:                  595Read operation >> data input = 128
+# KERNEL:                  626Write Operation >> Data Out = 1
+# KERNEL:                  646Write Operation >> Data Out = 2
+# KERNEL:                  666Write Operation >> Data Out = 4
+# KERNEL:                  686Write Operation >> Data Out = 8
+# KERNEL:                  706Write Operation >> Data Out = 16
+# KERNEL:                  726Write Operation >> Data Out = 32
+# KERNEL:                  746Write Operation >> Data Out = 64
+# KERNEL:                  766Write Operation >> Data Out = 128
+# RUNTIME: Info: RUNTIME_0068 testbench.sv (90): $finish called.
